7ecbbba1adfb99e197e84f6b9efb9a1adaba88b9
Joanna Wood <caitlinjoannawood@gmail.com>
2019-06-04 21:06:25 +0000

    added generateRandomString - works right now for lower case ASCII codes, add after capitals and 1-9

--- express_server.js
+++ express_server.js
@@ -4,12 +4,31 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+//! improve this to include 65-90 & 49-57
+function generateRandomString(){
+  let randomString = "";
+  let randomASCII;
+
+  for (let i = 0; i < 6; i++){
+    randomASCII = Math.floor((Math.random() * 25) + 97);
+    randomString += String.fromCharCode(randomASCII);
+  }
+  return randomString;
+}
+
 
 app.get("/", (req, res) => {
   res.send("Hey hello!");
@@ -25,6 +44,29 @@ app.get("/urls", (req, res) => {
 
   res.render("urls_index", { urls });
 });
+
+
+app.get("/urls/new", (req, res) => {
+
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+// app.get("/urls/:id", (req, res) => {
+//   let templateVars = {
+//     urls: urlDatabase
+//   };
+
+//   let urls = templateVars.urls;
+
+//   res.render("urls_index", {
+//     urls
+//   });
+// });
 
 app.get("/urls/:shortURL", (req, res) => {
   let shortURL =  req.params.shortURL;
