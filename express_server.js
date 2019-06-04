c3a2dfa9c6a9582172eee04358cc9fdf7dfe110e
Joanna Wood <caitlinjoannawood@gmail.com>
2019-06-04 17:22:17 +0000

    updated consts and lets, added urls route

--- express_server.js
+++ express_server.js
@@ -1,18 +1,25 @@
 
 
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
 
 app.set('view engine', 'ejs');
 
-var urlDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
 app.get("/", (req, res) => {
   res.send("Hey hello!");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = {
+    urls: urlDatabase
+  };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
